c34cfbd94a7b80d07824f8153e326cb4fe5277f6
soma <takeyabox84@gmail.com>
2026-05-02 01:41:25 +0900

    path

--- script.js
+++ script.js
@@ -15,23 +15,23 @@ const songs = [
     {
         id: 'music1',
         title: 'かわいいだけじゃだめですか？',
-        jacket: 'ui/jacket/jacket_music1.jpg',
+        jacket: 'jacket_music1.jpg',
         youtubeId: 'jZqTz1G8G04',
-        notesSrc: 'notes/music1.json'
+        notesSrc: 'music1.json'
     },
     {
         id: 'music2',
         title: 'わたしのいちばんかわいいところ',
-        jacket: 'ui/jacket/jacket_music2.jpg',
+        jacket: 'jacket_music2.jpg',
         youtubeId: 'NQUo3vITjgY',
-        notesSrc: 'notes/music2.json'
+        notesSrc: 'music2.json'
     },
     {
         id: 'music3',
         title: 'music3',
-        jacket: 'ui/jacket/jacket_music3.jpg',
+        jacket: 'jacket_music3.jpg',
         youtubeId: 'uySbsSsWBiE',
-        notesSrc: 'notes/music3.json'
+        notesSrc: 'music3.json'
     }
 ];
 let currentSongIndex = 0;
@@ -42,7 +42,7 @@ const NOTE_HEIGHT = 20;  // px, should match CSS --note-height
 const KEYS = ['f', 'g', 'h', 'j'];
 
 // Sound effect
-const hitSound = new Audio('sound/sound_notes.mp3');
+const hitSound = new Audio('sound_notes.mp3');
 hitSound.load();
 function playHitSound() {
     const s = hitSound.cloneNode();
